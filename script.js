89ecf8122dc3436e0b2292d4e4754f7c641508f4
FAYEZA-ZOBAIR <fayeza.zobair@gmail.com>
2024-02-05 18:11:19 +0530

    init

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+const modal=document.querySelector('modal');
+const overlay=document.querySelector('overlay');
+const btnOpenModal=document.querySelectorAll('show-modal');
+const btnCloseModal=document.querySelector('close-modal');
+
+const openModal= function() {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+const closeModal = function(){
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+for (let i=0; i<btnOpenModal.length+1; i++)
+    btnOpenModal[i].addEventListener('click', openModal);
+
+    btnCloseModal.addEventListener('click',closeModal)
+    overlay.addEventListener('click', closeModal)
